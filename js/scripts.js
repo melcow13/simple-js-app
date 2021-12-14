12dcffe44a8b2d52220108b719a2d5655254370f
Melody Kao <melody.ch.kao@gmail.com>
2021-12-14 20:39:24 +0100

    Update scripts.js
    add three different pokemon to the pokemon list including name, height and type.

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+let pokemonList [{name: "bulbasuar", height: 7, [types: "grass", "poison"},{name: "pikachu", height: 4, [types:"electric","mouse"]},{name: "charmander", height: 6, [types: "fire","lizard"]}]
