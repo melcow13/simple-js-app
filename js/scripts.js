4e7871b4c6f162e9ccdef2a691bb7790ff18e0c2
Melody Kao <melody.ch.kao@gmail.com>
2021-12-15 12:57:26 +0100

    Update scripts.js
    create indent and make each pokemon have their own line to make the code easily to read.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList [
+
+  {name: "bulbasuar", height: 7, types: ["grass", "poison"]},
+  {name: "pikachu", height: 4, types:["electric","mouse"]},
+  {name: "charmander", height: 6, types: ["fire","lizard"]}
+
+];
