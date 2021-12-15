4da21925eac65d4207a462cc6ff2d0f008b9d681
Melody Kao <melody.ch.kao@gmail.com>
2021-12-15 17:22:42 +0100

    my first three pokemon indented
    make it easy to read

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 let pokemonList=[
-
-  {name: "bulbasuar", height: 7, types: ["grass", "poison"]},
-  {name: "pikachu", height: 4, types:["electric","mouse"]},
-  {name: "charmander", height: 6, types: ["fire","lizard"]}
+  {
+    name: "Bulbasuar",
+    height: 7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Pikachu",
+    height: 4,
+    types:["electric","mouse"]
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    types: ["fire","lizard"]}
 
 ];
 
