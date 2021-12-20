054083cf104601e987b438a176671165e9f21bf7
Melody Kao <melody.ch.kao@gmail.com>
2021-12-20 20:38:35 +0100

    create the addListItemin the repository

--- js/scripts.js
+++ js/scripts.js
@@ -46,6 +46,16 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  };
+
   return {
     add: add,
     getAll: getAll
