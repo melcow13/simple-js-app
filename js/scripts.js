e4119b983da7d56c81b0a003834fa4f7c0dc4be8
Melody Kao <melody.ch.kao@gmail.com>
2021-12-20 12:19:05 +0100

    create a new pokemonRepository variable to hold what IIFE will return

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,34 @@
 
 }());
 
+let pokemonRepository = (function () {
+  let pokemonList=[
+    {
+      name: "Bulbasuar",
+      height: 7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Pikachu",
+      height: 4,
+      types:["electric","mouse"]
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ["fire","lizard"]}
+  ];
+
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
 pokemonList.forEach((pokemon) => {
   document.write ("<br>" + pokemon.name + " \("+ "Height:" + pokemon.height +"\)");
   if (pokemon.height > 6)
