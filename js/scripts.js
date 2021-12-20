04b31c42648d0d299bab53de0e6aba607b00721f
Melody Kao <melody.ch.kao@gmail.com>
2021-12-20 12:14:41 +0100

    wrap pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
-let pokemonList=[
-  {
-    name: "Bulbasuar",
-    height: 7,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Pikachu",
-    height: 4,
-    types:["electric","mouse"]
-  },
-  {
-    name: "Charmander",
-    height: 6,
-    types: ["fire","lizard"]}
+(function() {
 
-];
+  let pokemonList=[
+    {
+      name: "Bulbasuar",
+      height: 7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Pikachu",
+      height: 4,
+      types:["electric","mouse"]
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ["fire","lizard"]}
 
+  ];
+
+}());
 
 pokemonList.forEach((pokemon) => {
   document.write ("<br>" + pokemon.name + " \("+ "Height:" + pokemon.height +"\)");
