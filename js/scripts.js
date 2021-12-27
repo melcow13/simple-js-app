1bec6a2c04eb2fad0e0c46465a363cb185563780
Melody Kao <melody.ch.kao@gmail.com>
2021-12-27 16:15:04 +0100

    add function to the return value

--- js/scripts.js
+++ js/scripts.js
@@ -73,7 +73,8 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     showDetails: showDetails,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showModal: showModal
   };
 
 })();
