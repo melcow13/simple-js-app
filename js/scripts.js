768001de16e4e6f0dbba920ad7d654a99ff45644
Melody Kao <melody.ch.kao@gmail.com>
2021-12-27 20:04:48 +0100

    add content into the modal

--- js/scripts.js
+++ js/scripts.js
@@ -63,8 +63,25 @@ let pokemonRepository = (function () {
   });
   }
 
-  function showModal() {
+  function showModal(title, text) {
   let modalContainer = document.querySelector('#modal-container');
+  // Clear all existing modal content
+  modalContainer.innerHTML = '';
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Add the new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
   modalContainer.classList.add('is-visible');
   }
 
